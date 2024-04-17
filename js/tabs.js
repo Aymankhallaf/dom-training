9b2fda437b2fdaba4bb64d6db48d0d83a1839bc7
khallaf <engayman.khalaf@gmail.com>
2024-04-17 17:11:02 +0200

    tabs comments have been done

--- js/tabs.js
+++ js/tabs.js
@@ -1,39 +1,59 @@
-function getDataSetLst(tagtext) {
+/**
+ * gets all the data set of ul in Dom and puts in array.
+ * @param {string} dataSetToDisactive an id tag
+ * @returns {array} array of data set text.
+ */
+function getDataSetLst(dataSetToDisactive) {
   const datalst = [];
-  for (const child of document.getElementById(tagtext).children) {
+  for (const child of document.getElementById(dataSetToDisactive).children) {
     datalst.push(child.querySelector("a").dataset.tab);
   }
   return datalst;
 }
 
-function getArticls(dataSetTag) {
+/**
+ * gets an articles( or an element) by id tag.
+ * @param {string} dataSetTag an id tag.
+ * @returns {element} an element of Dom.
+ */
+function getArticle(dataSetTag) {
   return document.getElementById(dataSetTag);
 }
+
+/**
+ * gets a link (or an element ) by data set
+ * "#tabs-links [data-tab='" + dataSetTag + "']"
+ * @param {string} dataSetTag data text set.
+ * @returns {element} an element of Dom.
+ */
 function getLink(dataSetTag) {
   return document.querySelector("#tabs-links [data-tab='" + dataSetTag + "']");
 }
 
-function hideArticlesExcepetOne(tagtext, showSet) {
-  for (const set of getDataSetLst(tagtext)) {
-    getArticls(set).classList.add("text-hidden");
+/**
+ * active tab and show the text of this tab.
+ * @param {string} dataSetToDisactive a tag text to disactive.
+ * @param {srting} dataSetToActive a tag text to active.
+ */
+function activeArticle(dataSetToDisactive, dataSetToActive) {
+  for (const set of getDataSetLst(dataSetToDisactive)) {
+    getArticle(set).classList.add("text-hidden");
     getLink(set).classList.remove("active");
   }
-  getArticls(showSet).classList.remove("text-hidden");
-  getLink(showSet).classList.add("active");
+  getArticle(dataSetToActive).classList.remove("text-hidden");
+  getLink(dataSetToActive).classList.add("active");
 }
 
-
-const tabsLinks = document.getElementById("tabs-links").children;
-
-function activeTab() {
-  for (const child of tabsLinks) {
-    child.classList.remove("active");
+/**
+ * actives and disactives tab.
+ * @param {*} ulId the UL id tag.
+ */
+function activeTab(ulId) {
+  for (const child of document.getElementById(ulId).children) {
     child.addEventListener("click", function (event) {
-      event.target.classList.add("active");
-      hideArticlesExcepetOne("tabs-links", event.target.dataset.tab);
+      activeArticle(ulId, event.target.dataset.tab);
     });
   }
 }
 
-activeTab();
-
+activeTab("tabs-links");
