0aaf58c1f886aaa7caceeecd9afa8c3d107cbd6a
khallaf <engayman.khalaf@gmail.com>
2024-04-18 09:10:54 +0200

    show function done

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,65 @@
+const ingredients = [
+  "🍅",
+  "🥑",
+  "🥕",
+  "🌽",
+  "🌶️",
+  "🥦",
+  "🥒",
+  "🍄",
+  "🧄",
+  "🧅",
+  "🧀",
+  "🍠",
+  "🥚",
+  "🥬",
+];
 
-const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+function createLiBtnTemplate(item, targetUlId) {
+  document.getElementById(targetUlId).innerHTML +=
+    "<li class=" +
+    '"' +
+    "js-li-" +
+    targetUlId +
+    '"' +
+    ">" +
+    "<button class=" +
+    '".button ' +
+    "js-btn-" +
+    targetUlId +
+    '"' +
+    ">" +
+    item +
+    "</button></li>";
+}
+
+function displayArrayInliAndButton(array, targetUlId) {
+  for (const item of array) {
+    createLiBtnTemplate(item, targetUlId);
+  }
+}
+
+const allIngredients = displayArrayInliAndButton(
+  ingredients,
+  "salad-ingredients"
+);
+
+function getClickedHtmlTag() {}
+
+function addLiToUl() {
+  document
+    .getElementById("salad-ingredients")
+    .addEventListener("click", function (event) {
+      if (document.getElementById("salad-final").children.length >= 5) return;
+      createLiBtnTemplate(event.target.innerHTML, "salad-final");
+      document.getElementById("salad-count").innerText = document.getElementById("salad-final").children.length
+    });
+}
+
+addLiToUl();
+
+// for (const item of allIngredients) {
+//   item.addEventListener("click", function (event) {
+//     console.log(event);
+//   });
+// }
